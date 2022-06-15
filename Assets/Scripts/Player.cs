671b98c48553e4f29f51433b4824829351b2d8ec
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-06-15 19:04:25 +0700

    Pllying first person Camera to my player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,10 @@ public class Player : MonoBehaviour
 
     public float speed = 12.5f;
     public CharacterController myController;
+    [SerializeField]private Transform myCameraHead;
+
+    public float mouseSensitivity = 100f;
+    private float cameraVerticalRotation;
 
 
     // Start is called before the first frame update
@@ -19,6 +23,15 @@ public class Player : MonoBehaviour
     void Update()
     {
         PlayerMovement();
+
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        cameraVerticalRotation -= mouseY;
+        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, 90f, 90f);
+
+        transform.Rotate(Vector3.up * mouseX);   
+        myCameraHead.localRotation = Quaternion.Euler(cameraVerticalRotation, 0f, 0f);
     }
 
     void PlayerMovement()
